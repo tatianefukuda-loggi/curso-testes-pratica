06ba203d6d43b2505baa55fda7bebdc80f873206
Ana <ee.anap@gmail.com>
2020-02-11 09:48:40 -0300

    phone tests

--- loggi/config/settings.py
+++ loggi/config/settings.py
@@ -15,7 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-LOCAL_APPS = ['cx']
+LOCAL_APPS = [
+    'cx',
+    'base',
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
